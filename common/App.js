051adddc81b7f2399c372c15612228db0d83bcb5
Federico Hernández Velásquez <federicohernandez.ve@gmail.com>
2020-06-12 03:25:10 -0700

    Extracted styles to make an example with css file and styled components on ssr.

--- common/App.js
+++ common/App.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react'
+import styled from 'styled-components';
+import './styles.css';
+
+const StyledBox = styled.div`
+	height: 100px;
+	background: black;
+	color: yellow;
+	font-size: 40px;
+`;
+
 
 class App extends Component {
 
@@ -26,10 +36,13 @@ class App extends Component {
 
 	render(){
 		return (
-			<div style={{ background: 'purple', color: 'white', fontSize: '30px' }}>
+			<div className="container">
 				<div>Message from the server: {this.state.message}</div>
 				<br/>
 				<button onClick={this.getMessage}>Get Message</button>
+				<StyledBox>
+					This is just a box that will bring styles from server and also hot reload its styles on every change
+				</StyledBox>
 			</div>
 		)
 	}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 import api from './api';
-// import hotroute from './hotroute';
+import hotroute from './hotroute';
 
 function applyRoutes(app) {
 	app.use(api.routes()).use(api.allowedMethods());
-	// app.use(hotroute.routes()).use(hotroute.allowedMethods());
+	app.use(hotroute.routes()).use(hotroute.allowedMethods());
 }
 
 export default applyRoutes;
